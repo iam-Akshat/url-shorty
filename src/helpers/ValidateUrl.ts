e0cd59b7fed3defe9d26ecbe1992c57c7c1119f6
Akshat <masterakshata@gmail.com>
2021-01-14 01:37:59 +0530

    skeleton form for creating urls

---
+++ src/helpers/ValidateUrl.ts
@@ -0,0 +1,12 @@
+const URLRegexPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+
+const URLregex = new RegExp(URLRegexPattern)
+
+const isValidUrl = (url:string) =>{
+    if (url.match(URLregex)) {
+        return true
+    }
+
+    return false
+}
+export { isValidUrl }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
 import express from "express";
 import redirects from "./routes/Redirects";
+import { createURL } from "./routes/CreateURL";
 import { dbConnect } from "./db/connectDb";
 import dotenv from "dotenv";
+import { join } from "path";
+
+const port  = process.env.PORT || 3000
 
 dotenv.config({path:'E:/url-short/.env'})
+
 const app = express()
+
+//Config
 app.set('view engine', 'ejs');
-app.use(express.static('./public'))
-const port  = process.env.PORT || 3000
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({extended:true}))
+
+// Routes
 app.use('/',redirects)
+app.use('/',createURL)
+
 app.listen(port, async () => {
     await dbConnect(process.env.MONGO_URI!)
     return console.log(`server is listening on ${port}`);
