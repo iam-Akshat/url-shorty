1bf8bd7564707925a24de99a62ed3dcb7c85de3b
Akshat <masterakshata@gmail.com>
2021-01-28 04:11:53 +0530

    add styles and navbar

--- public/index.js
+++ public/index.js
@@ -1,13 +1,17 @@
 /* eslint-disable no-undef */
+
+//  FOR BATCH URLS PAGE
 const f_tags=[]
 const tags = document.getElementById("tags")
 const holder = document.getElementById("tag_holder")
 const form = document.getElementById("batch")
 console.log(tags);
-tags.addEventListener("keydown", (event) => {
+const batchtags = () =>{
+    tags.addEventListener("keydown", (event) => {
+        if (event.code == "Space" && tags.value.trim().length != 0) { createTag() }
+    })
+}
 
-    if (event.code == "Space" && tags.value.trim().length != 0) { createTag() }
-})
 const tagBuilder = (content) => {
     return (
         `<div id="tag">${content}</div>`
@@ -39,14 +43,41 @@ const commonFetch = async (url,body,isJSON=true) => {
         return res
     }
 }
-batch.addEventListener("submit",async (e)=>{
-    e.preventDefault()
-    const ff = new FormData(form)
-    const tags_str = f_tags.join(",")
-    ff.append('tags',tags_str)
-    const form_encoded = new URLSearchParams(ff)
-
-    const res = await commonFetch('/batch',form_encoded,false)
-    console.log(res);
+const batchListener = () => {
+    batch.addEventListener("submit",async (e)=>{
+        e.preventDefault()
+        const ff = new FormData(form)
+        const tags_str = f_tags.join(",")
+        ff.append('tags',tags_str)
+        const form_encoded = new URLSearchParams(ff)
     
-})
+        const res = await commonFetch('/batch',form_encoded,false)
+        console.log(res);
+    })
+}
+//  FOR BATCH URLS PAGE
+
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
