c79700c9b1c577a58ef3817be4c1f0a784f85fad
Akshat <masterakshata@gmail.com>
2021-01-29 14:51:39 +0530

    fix same id generated bug

--- src/models/Link.ts
+++ src/models/Link.ts
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 
 const LinkSchema: Schema = new Schema({
     full_url: { type: String, required: true },
-    short_url: { type: String, default: nanoid(6) },
+    short_url: { type: String, default: '' },
     tag: { type: String, null: true },
     clicks: { type: Number, default: 0 },
     batch_id: { type: String }
@@ -20,7 +20,7 @@ const Link: Model<ILink> = model('Link', LinkSchema)
 
 const createLinkHelper = async (full_url: string, tag?: string, batch_id?: string): Promise<ILink> => {
 
-    const newShortUrl: ILink = await Link.create({ full_url: full_url })
+    const newShortUrl: ILink = await Link.create({ full_url: full_url,short_url:nanoid((Math.ceil(Math.random()*2) -1)+6) })
 
     if (tag) {
         newShortUrl.tag = tag
