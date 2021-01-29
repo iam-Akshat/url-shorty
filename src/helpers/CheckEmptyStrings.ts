062b98751fc50531bec948f9fbe562cbe6e31acc
Akshat <masterakshata@gmail.com>
2021-01-30 02:37:44 +0530

    fix tsc errors

--- src/helpers/CheckEmptyStrings.ts
+++ src/helpers/CheckEmptyStrings.ts
@@ -1,10 +1,17 @@
-const isEmptyStrings = (str : string | string[]): boolean => {
+const isEmptyStrings = (str: string | string[]): boolean => {
     if (typeof (str) == "object") {
+        let flag = false
         str.forEach(s => {
-            if (s.trim().length == 0) return false
+            if (s.trim().length == 0) {
+                flag = true
+            }
         })
-        return true
-    }else{
+        if (flag) {
+            return false
+        } else {
+            return true
+        }
+    } else {
         if (str.trim().length == 0) return false
     }
     return true
